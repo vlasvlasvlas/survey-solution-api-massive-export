7c5181d5e5b93f71cdb883e84e6e157687698ca8
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2023-08-24 10:26:50 -0300

    readme, dot, credential vars

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Just a simple masive export tabular data download for Survey Solutions using an 
 
 It uses ssaw library : https://github.com/vavalomi/ssaw
 
+# Reqs:
+
+- Python3
+
+- ssaw
+
+- must install pydantic==1.10.11 or it will not work (issue opened at ssaw) 
+
+- or you can just pip install -r requirements!
+
--- export_masivo_ssolution.py
+++ export_masivo_ssolution.py
@@ -1,4 +1,7 @@
-import ssaw,os
+import ssaw, os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 """
 
@@ -14,14 +17,13 @@ or:
 
 """
 
-# parameters
-host='http://127.0.0.1:9700'
-api_user='apiuser1'
-api_password='Abcde12345'
-workspace='primary'
+# parameters, changed them to match yours
+# change .env (clone it from .env.dummy)
 
 # First, initialize connection with the server
-client = ssaw.Client(host, api_user=api_user, api_password=api_password, workspace=workspace)
+client = ssaw.Client(
+    os.getenv("DB_HOST"), api_user=os.getenv("DB_USER"), api_password=os.getenv("DB_PWRD"), workspace=os.getenv("SS_WORK")
+)
 
 
 # Get list of questionnaires
@@ -33,9 +35,9 @@ for q in ssaw.QuestionnairesApi(client).get_list():
     final_directory = os.path.join(current_directory, q.id)
     if not os.path.exists(final_directory):
         os.makedirs(final_directory)
-    
-    print("folder:",final_directory)
-    # Download the latest exported data in Tabular format
-    filename = ssaw.ExportApi(client).get(export_type="Tabular",export_path=final_directory,
-    questionnaire_identity=q.id)
 
+    print("folder:", final_directory)
+    # Download the latest exported data in Tabular format
+    filename = ssaw.ExportApi(client).get(
+        export_type="Tabular", export_path=final_directory, questionnaire_identity=q.id
+    )
